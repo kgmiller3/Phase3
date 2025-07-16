fa5c953b6256bdec7a761efea29593b97d407d4a
kgmiller3 <kgmiller2@yahoo.com>
2025-07-16 15:12:28 -0400

    stage09-optional01-base-complete

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const da = require("./data-access");
 const bodyParser = require('body-parser');
+const apiKey = process.env.API_KEY;
 
 
 const app = express();
@@ -12,9 +13,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+console.log('API Key from environment:', apiKey);
 });
 
-app.get('/customers', async (req, res) => {
+function validateApiKey(req, res, next) {
+    let api_key = req.headers['x-api-key'];
+    console.log('Received API Key:', api_key);
+    console.log('API Key from environment:', process.env.API_KEY);
+    if (!api_key){
+        console.error('API key is missing');
+        res.status(401).send('API key is missing');
+        return;
+    } 
+    if( api_key !== process.env.API_KEY) {
+        console.error('Invalid API key');
+        res.status(401).send('Invalid API key');
+        return; 
+    }
+
+    console.log('API Key:', api_key);
+    next();
+}
+
+
+app.get('/customers', validateApiKey, async (req, res) => {
     const [customers, error] = await da.getCustomers();
     if(customers) {
       res.send(customers);
@@ -25,7 +47,7 @@ app.get('/customers', async (req, res) => {
   }
 });
 
-app.get('/reset', async (req, res) => {
+app.get('/reset', validateApiKey, async (req, res) => {
     const [message, error] = await da.restCustomers();
     if(message) {
       res.send(message);
@@ -36,7 +58,7 @@ app.get('/reset', async (req, res) => {
   }
 });
 
-app.get('/customers/:id', async (req, res) => {
+app.get('/customers/:id', validateApiKey, async (req, res) => {
     const id = req.params.id;
     const [customer, error] = await da.getCustomerById(id);
     if (customer) {
@@ -50,7 +72,7 @@ app.get('/customers/:id', async (req, res) => {
    
 });
 
-app.post('/customers', async (req, res) => {
+app.post('/customers', validateApiKey, async (req, res) => {
     if (!req.body) {
         res.status(400).send('No customer data provided');
         return;
@@ -71,7 +93,7 @@ app.post('/customers', async (req, res) => {
     }
 });
 
-app.put('/customers/:id', async (req, res) => {
+app.put('/customers/:id', validateApiKey, async (req, res) => {
     
     const id = req.params.id;
     const updatedCustomer = req.body;
@@ -97,7 +119,7 @@ app.put('/customers/:id', async (req, res) => {
             }
         });
 
-app.delete('/customers/:id', async (req, res) => {
+app.delete('/customers/:id', validateApiKey, async (req, res) => {
     const id = req.params.id;
     try {
         const [message, error] = await da.deleteCustomer(id);
